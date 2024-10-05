62f26942cb73be61771d6ba4a7f7a52bb14d3927
Dimi Racordon <dimitri.racordon@gmail.com>
2024-10-05 08:46:17 +0200

    Add 'StableSet'

---
+++ Sources/MoreCollections/StableSet.swift
@@ -0,0 +1,146 @@
+/// A hash set that preserves the positions of its elements on mutation.
+public struct StableSet<Element: Hashable> {
+
+  /// An empty value.
+  private struct Empty: Hashable {}
+
+  /// The contents of the set.
+  private var contents: StableDictionary<Element, Empty>
+
+  /// Creates an empty instance.
+  public init() {
+    self.contents = [:]
+  }
+
+  /// Creates an empty instance with enough space to store `minimumCapacity` elements without
+  /// allocating new storage.
+  public init(minimumCapacity: Int) {
+    self.contents = .init(minimumCapacity: minimumCapacity)
+  }
+
+  /// Creates an instance with the elements in `members`.
+  public init<S: Sequence<Element>>(_ members: S) {
+    self.contents = .init(uniqueKeysAndValues: members.lazy.map({ ($0, .init()) }))
+  }
+
+  /// `true` iff `self` is empty.
+  public var isEmpty: Bool {
+    count == 0
+  }
+
+  /// The number of elements stored in `self`.
+  public var count: Int {
+    contents.count
+  }
+
+  /// The number of elements that can be stored in `self` without allocating new storage.
+  public var capacity: Int {
+    contents.capacity
+  }
+
+  /// Returns the position of `member` in `self`.
+  public func firstIndex(of member: Element) -> Int? {
+    contents.index(forKey: member)
+  }
+
+  /// Returns `true` iff `member` is contained in `self`.
+  public func contains(_ member: Element) -> Bool {
+    contents.index(forKey: member) != nil
+  }
+
+  /// Adds `member` to `self` if it is not already present and returns `(inserted: i, position: p)`
+  /// where `i` is `true` iff the insertion occured and `p` is the position of `member` in `self`.
+  @discardableResult
+  public mutating func insert(_ member: Element) -> (inserted: Bool, position: Int) {
+    contents.assignValue(.init(), forKey: member)
+  }
+
+  /// Removes the element stored at `p`.
+  ///
+  /// - Requires: `p` is a valid position in `self`.
+  @discardableResult
+  public mutating func remove(at p: Int) -> Element {
+    contents.remove(at: p).key
+  }
+
+  /// Removes `member` and returns `true` iff it was present; otherwise, returns `false`.
+  @discardableResult
+  public mutating func remove(_ member: Element) -> Bool {
+    contents.removeValue(forKey: member) != nil
+  }
+
+  /// Removes all elements in `self`, preserving storage if `keepCapacity` is `true`.
+  public mutating func removeAll(keepingCapacity keepCapacity: Bool = false) {
+    contents.removeAll(keepingCapacity: keepCapacity)
+  }
+
+  /// Reserves enough space to store `minimumCapacity` elements without allocating new storage.
+  public mutating func reserveCapacity(_ minimumCapacity: Int) {
+    contents.reserveCapacity(minimumCapacity)
+  }
+
+}
+
+extension StableSet: ExpressibleByArrayLiteral {
+
+  /// Creates an instance from an array literal.
+  public init(arrayLiteral members: Element...) {
+    self.init(members)
+  }
+
+}
+
+extension StableSet: Collection {
+
+  public typealias Index = Int
+
+  /// The position of the first key/value pair in `self`.
+  public var startIndex: Int {
+    contents.startIndex
+  }
+
+  /// The position immediately after the position of the last key/value pair in `self`.
+  public var endIndex: Int {
+    contents.endIndex
+  }
+
+  /// Returns the position immediately after `p`.
+  ///
+  /// - Requires: `p` is a valid position in `self`.
+  public func index(after p: Int) -> Int {
+    contents.index(after: p)
+  }
+
+  /// Accesses the element at `p`.
+  ///
+  /// - Requires: `p` is a valid position in `self`.
+  public subscript(p: Int) -> Element {
+    contents[p].key
+  }
+
+}
+
+extension StableSet: BidirectionalCollection {
+
+  /// Returns the position immediately before `p`.
+  ///
+  /// - Requires: `p` is a valid position in `self` and different from `self.startIndex`.
+  public func index(before i: Int) -> Int {
+    contents.index(before: i)
+  }
+
+}
+
+extension StableSet: Equatable where Element: Equatable {}
+
+extension StableSet: Hashable where Element: Hashable {}
+
+extension StableSet: CustomStringConvertible {
+
+  /// A textual description of `self`.
+  public var description: String {
+    let members = self.map({ (k) in "\(k)" }).joined(separator: ", ")
+    return "[\(members)]"
+  }
+
+}
---
+++ Tests/MoreCollectionsTests/StableSetTests.swift
@@ -0,0 +1,187 @@
+import XCTest
+import MoreCollections
+
+class StableSetTests: XCTestCase {
+
+  func testIsEmpty() {
+    var s = StableSet<String>()
+    XCTAssert(s.isEmpty)
+    s.insert("a")
+    XCTAssertFalse(s.isEmpty)
+  }
+
+  func testCount() {
+    var s = StableSet<String>()
+    XCTAssertEqual(s.count, 0)
+    s.insert("a")
+    XCTAssertEqual(s.count, 1)
+    s.insert("b")
+    XCTAssertEqual(s.count, 2)
+    s.remove("b")
+    XCTAssertEqual(s.count, 1)
+    s.remove("a")
+    XCTAssertEqual(s.count, 0)
+  }
+
+  func testInitWithSequence() {
+    let members = ["a", "b"]
+    let s = StableSet<String>(members)
+    XCTAssert(s.elementsEqual(members))
+  }
+
+  func testInitWithArrayLiteral() {
+    let s: StableSet = ["a", "b"]
+    XCTAssert(s.elementsEqual(["a", "b"]))
+  }
+
+  func testFirstIndexOf() {
+    var s = StableSet<String>()
+    XCTAssertNil(s.firstIndex(of: "a"))
+
+    s.insert("a")
+    XCTAssertEqual(s.firstIndex(of: "a"), 0)
+
+    s.insert("b")
+    s.insert("c")
+    XCTAssertEqual(s.firstIndex(of: "a"), 0)
+    XCTAssertEqual(s.firstIndex(of: "b"), 1)
+    XCTAssertEqual(s.firstIndex(of: "c"), 2)
+
+    s.remove("b")
+    XCTAssertEqual(s.firstIndex(of: "a"), 0)
+    XCTAssertNil(s.firstIndex(of: "b"))
+    XCTAssertEqual(s.firstIndex(of: "c"), 2)
+  }
+
+
+  func testContains() {
+    var s = StableSet<String>()
+    XCTAssertFalse(s.contains("a"))
+    s.insert("a") // insert
+    XCTAssert(s.contains("a"))
+    s.remove("a") // delete
+    XCTAssertFalse(s.contains("a"))
+    s.remove("a") // no-op
+    XCTAssertFalse(s.contains("a"))
+  }
+
+
+  func testInsert() {
+    var s = StableSet<String>()
+
+    let p0 = s.insert("a")
+    XCTAssert(p0.inserted)
+    XCTAssertEqual(p0.position, 0)
+    XCTAssertEqual(s[p0.position], "a")
+
+    let p1 = s.insert("a")
+    XCTAssertFalse(p1.inserted)
+    XCTAssertEqual(p1.position, 0)
+    XCTAssertEqual(s[p0.position], "a")
+
+    let p2 = s.insert("b")
+    XCTAssert(p2.inserted)
+    XCTAssertEqual(p2.position, 1)
+    XCTAssertEqual(s[p2.position], "b")
+  }
+
+  func testReinsert() {
+    var s: StableSet = ["a", "b", "c"]
+    s.remove("b")
+    s.insert("b")
+    XCTAssertEqual(s.firstIndex(of: "b"), 1)
+  }
+
+  func testRemoveAt() {
+    var s: StableSet = ["a", "b", "c"]
+    XCTAssertEqual(s.remove(at: 1), "b")
+    XCTAssertEqual(Array(s), ["a", "c"])
+    XCTAssertEqual(s.remove(at: 2), "c")
+    XCTAssertEqual(Array(s), ["a"])
+  }
+
+  func testRemoveMember() {
+    var s: StableSet = ["a", "b", "c"]
+    XCTAssert(s.remove("a"))
+    XCTAssertFalse(s.remove("a"))
+    XCTAssertFalse(s.remove("z"))
+  }
+
+  func testRemoveAll() {
+    var s: StableSet = ["a", "b", "c"]
+    s.removeAll()
+    XCTAssert(s.isEmpty)
+
+    s.insert("a")
+    s.removeAll(keepingCapacity: true)
+    XCTAssert(s.isEmpty)
+    XCTAssertGreaterThanOrEqual(s.capacity, 1)
+
+    s.insert("a")
+    let t = s
+    s.removeAll(keepingCapacity: true)
+    XCTAssert(t.contains("a"))
+    XCTAssert(s.isEmpty)
+    XCTAssertGreaterThanOrEqual(s.capacity, 1)
+  }
+
+  func testReserveCapacity() {
+    var s: StableSet = ["a", "b", "c"]
+    let t = s
+    s.reserveCapacity(100)
+    XCTAssertEqual(s, t)
+    XCTAssertNotEqual(s.capacity, t.capacity)
+    XCTAssertGreaterThanOrEqual(s.capacity, 100)
+  }
+
+  func testIsValue() {
+    let s: StableSet = ["a", "b", "c"]
+    var t = s
+    t.insert("d")
+    XCTAssert(s.contains("a"))
+    XCTAssert(t.contains("a"))
+  }
+
+  func testIsCollection() {
+    var s: StableSet = ["a", "b", "c"]
+    s.remove(at: 1)
+
+    var i = s.startIndex
+    XCTAssertEqual(s[i], "a")
+    i = s.index(after: i)
+    XCTAssertEqual(s[i], "c")
+    i = s.index(after: i)
+    XCTAssertEqual(i, s.endIndex)
+  }
+
+  func testIsBidirectionalCollection() {
+    var s: StableSet = ["a", "b", "c"]
+    s.remove(at: 1)
+
+    var i = s.index(before: s.endIndex)
+    XCTAssertEqual(s[i], "c")
+    i = s.index(before: i)
+    XCTAssertEqual(s[i], "a")
+  }
+
+  func testIsEquatable() {
+    let s: StableSet = ["a", "b", "c"]
+    let t: StableSet = ["a", "b", "c"]
+    XCTAssertEqual(s, s)
+    XCTAssertEqual(s, t)
+    let u: StableSet = ["c", "b", "a"]
+    XCTAssertNotEqual(s, u)
+  }
+
+  func testIsHashable() {
+    let s: StableSet = ["a", "b", "c"]
+    let t: StableSet = ["a", "b", "c"]
+    XCTAssertEqual(s.hashValue, t.hashValue)
+  }
+
+  func testIsCustomStringConvertible() {
+    let s: StableSet = [1, 2, 3]
+    XCTAssertEqual(s.description, "[1, 2, 3]")
+  }
+
+}
