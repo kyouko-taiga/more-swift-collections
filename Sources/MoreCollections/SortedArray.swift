7beace82688daa78507f427de4b5d6e7bbdeab80
Dimi Racordon <dimitri.racordon@gmail.com>
2024-10-11 21:20:26 +0200

    Add 'SortedArray'

---
+++ Sources/MoreCollections/SortedArray.swift
@@ -0,0 +1,159 @@
+/// A sorted, random-access collection.
+public struct SortedArray<Element: Comparable> {
+
+  /// The contents of the array.
+  private var contents: [Element]
+
+  /// Creates an empty array.
+  public init() {
+    self.contents = []
+  }
+
+  /// Creates an instance with the elements in `members`.
+  public init<S: Sequence<Element>>(_ members: S) {
+    self.contents = members.sorted()
+  }
+
+  /// The number of elements stored in `self`.
+  public var count: Int {
+    contents.count
+  }
+
+  /// The number of elements that can be stored in `self` without allocating new storage.
+  public var capacity: Int {
+    contents.capacity
+  }
+
+  /// Returns `true` iff `element` is contained in `self`.
+  ///
+  /// - Complexity: O(log *n*), where *n* is the length of `self`.
+  public func contains(_ element: Element) -> Bool {
+    let p = insertionIndex(of: element)
+    return (p < contents.count) && (contents[p] == element)
+  }
+
+  /// Inserts `newElement` in `self` and returns its position.
+  ///
+  /// - Complexity: O(log *n*), where *n* is the length of `self`.
+  @discardableResult
+  public mutating func insert(_ newElement: Element) -> Int {
+    let p = insertionIndex(of: newElement)
+    contents.insert(newElement, at: p)
+    return p
+  }
+
+  /// Removes the element stored at `p`.
+  ///
+  /// - Requires: `p` is a valid position in `self`.
+  @discardableResult
+  public mutating func remove(at p: Int) -> Element {
+    contents.remove(at: p)
+  }
+
+  /// Removes all elements in `self`, preserving storage if `keepCapacity` is `true`.
+  public mutating func removeAll(keepingCapacity keepCapacity: Bool = false) {
+    contents.removeAll(keepingCapacity: keepCapacity)
+  }
+
+  /// Reserves enough space to store `minimumCapacity` elements without allocating new storage.
+  public mutating func reserveCapacity(_ minimumCapacity: Int) {
+    contents.reserveCapacity(minimumCapacity)
+  }
+
+  /// Returns the position at which `member` could be inserted.
+  private func insertionIndex(of element: Element) -> Int {
+    var upper = contents.count
+    var lower = 0
+
+    while upper > 0 {
+      let h = upper >> 1
+      let m = lower + h
+      if element <= contents[m] {
+        upper = h
+      } else {
+        lower = m + 1
+        upper = upper - (h + 1)
+      }
+    }
+
+    return lower
+  }
+
+}
+
+extension SortedArray: ExpressibleByArrayLiteral {
+
+  /// Creates an instance from an array literal.
+  public init(arrayLiteral members: Element...) {
+    self.init(members)
+  }
+
+}
+
+extension SortedArray: Collection {
+
+  public typealias Index = Int
+
+  /// The position of the first element in `self`.
+  public var startIndex: Int {
+    contents.startIndex
+  }
+
+  /// The position immediately after the position of the last element in `self`.
+  public var endIndex: Int {
+    contents.endIndex
+  }
+
+  /// Returns the position immediately after `p`.
+  ///
+  /// - Requires: `p` is a valid position in `self`.
+  public func index(after p: Int) -> Int {
+    contents.index(after: p)
+  }
+
+  /// Accesses the element at `p`.
+  ///
+  /// - Requires: `p` is a valid position in `self`.
+  public subscript(p: Int) -> Element {
+    _read { yield contents[p] }
+  }
+
+}
+
+extension SortedArray: BidirectionalCollection {
+
+  /// Returns the position immediately before `p`.
+  ///
+  /// - Requires: `p` is a valid position in `self` and different from `self.startIndex`.
+  public func index(before p: Int) -> Int {
+    contents.index(before: p)
+  }
+
+}
+
+extension SortedArray: RandomAccessCollection {
+
+  /// Returns the position at offset `d` from `p`.
+  public func index(_ p: Int, offsetBy d: Int) -> Int {
+    contents.index(p, offsetBy: d)
+  }
+
+  /// Returns the position at offset `d` from `p`, or `limit` that position is beyond `endIndex`.
+  public func index(_ p: Int, offsetBy d: Int, limitedBy limit: Int) -> Int? {
+    contents.index(p, offsetBy: d, limitedBy: limit)
+  }
+
+}
+
+extension SortedArray: Equatable {}
+
+extension SortedArray: Hashable where Element: Hashable {}
+
+extension SortedArray: CustomStringConvertible {
+
+  /// A textual description of `self`.
+  public var description: String {
+    contents.description
+  }
+
+}
---
+++ Tests/MoreCollectionsTests/SortedArrayTest.swift
@@ -0,0 +1,140 @@
+import XCTest
+import MoreCollections
+
+class SortedArrayTests: XCTestCase {
+
+  func testIsEmpty() {
+    var s = SortedArray<Int>()
+    XCTAssert(s.isEmpty)
+    s.insert(0)
+    XCTAssertFalse(s.isEmpty)
+  }
+
+  func testCount() {
+    var s = SortedArray<Int>()
+    XCTAssertEqual(s.count, 0)
+    s.insert(0)
+    XCTAssertEqual(s.count, 1)
+    s.insert(1)
+    XCTAssertEqual(s.count, 2)
+    s.remove(at: 0)
+    XCTAssertEqual(s.count, 1)
+  }
+
+  func testInitWithSequence() {
+    let s = SortedArray([1, 5, 3])
+    XCTAssert(s.elementsEqual([1, 3, 5]))
+  }
+
+  func testInitWithArrayLiteral() {
+    let s: SortedArray = [1, 5, 3]
+    XCTAssert(s.elementsEqual([1, 3, 5]))
+  }
+
+
+  func testContains() {
+    var s = SortedArray<Int>()
+    XCTAssertFalse(s.contains(42))
+    s.insert(42) // insert
+    XCTAssert(s.contains(42))
+    s.remove(at: 0) // delete
+    XCTAssertFalse(s.contains(42))
+  }
+
+
+  func testInsert() {
+    var s = SortedArray<Int>()
+    XCTAssertEqual(s.insert(0), 0)
+    XCTAssertEqual(s.insert(0), 0)
+    XCTAssertEqual(s.insert(2), 2)
+    XCTAssertEqual(s.insert(1), 2)
+  }
+
+  func testRemoveAt() {
+    var s: SortedArray = [1, 5, 3]
+    XCTAssertEqual(s.remove(at: 1), 3)
+    XCTAssertEqual(Array(s), [1, 5])
+  }
+
+  func testRemoveAll() {
+    var s: SortedArray = [1, 5, 3]
+    s.removeAll()
+    XCTAssert(s.isEmpty)
+
+    s.insert(0)
+    s.removeAll(keepingCapacity: true)
+    XCTAssert(s.isEmpty)
+    XCTAssertGreaterThanOrEqual(s.capacity, 1)
+
+    s.insert(0)
+    let t = s
+    s.removeAll(keepingCapacity: true)
+    XCTAssert(t.contains(0))
+    XCTAssert(s.isEmpty)
+    XCTAssertGreaterThanOrEqual(s.capacity, 1)
+  }
+
+  func testReserveCapacity() {
+    var s: SortedArray = [1, 5, 3]
+    let t = s
+    s.reserveCapacity(100)
+    XCTAssertEqual(s, t)
+    XCTAssertNotEqual(s.capacity, t.capacity)
+    XCTAssertGreaterThanOrEqual(s.capacity, 100)
+  }
+
+  func testIsValue() {
+    let s: SortedArray = [1, 5, 3]
+    var t = s
+    t.insert(4)
+    XCTAssert(s.contains(1))
+    XCTAssert(t.contains(1))
+  }
+
+  func testIsCollection() {
+    let s: SortedArray = [1, 5]
+    let i = s.startIndex
+    XCTAssertEqual(s[i], 1)
+    let j = s.index(after: i)
+    XCTAssertEqual(s[j], 5)
+    let k = s.index(after: j)
+    XCTAssertEqual(k, s.endIndex)
+  }
+
+  func testIsBidirectionalCollection() {
+    let s: SortedArray = [1, 5]
+    let i = s.index(before: s.endIndex)
+    XCTAssertEqual(s[i], 5)
+    let j = s.index(before: i)
+    XCTAssertEqual(s[j], 1)
+  }
+
+  func testIsRandomAccessCollection() {
+    let s: SortedArray = [1, 5, 3, 7]
+    let i = s.index(s.startIndex, offsetBy: 2)
+    XCTAssertEqual(i, 2)
+    let j = s.index(i, offsetBy: 8, limitedBy: 3)
+    XCTAssertNil(j)
+  }
+
+  func testIsEquatable() {
+    let s: SortedArray = [1, 5, 3]
+    let t: SortedArray = [1, 5, 3]
+    XCTAssertEqual(s, s)
+    XCTAssertEqual(s, t)
+    let u: SortedArray = [1, 5, 3, 7]
+    XCTAssertNotEqual(s, u)
+  }
+
+  func testIsHashable() {
+    let s: SortedArray = [1, 5, 3]
+    let t: SortedArray = [1, 5, 3]
+    XCTAssertEqual(s.hashValue, t.hashValue)
+  }
+
+  func testIsCustomStringConvertible() {
+    let s: SortedArray = [1, 5, 3]
+    XCTAssertEqual(s.description, "[1, 3, 5]")
+  }
+
+}
